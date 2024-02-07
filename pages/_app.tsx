c57f6d068d72dbb69395c240495014bae3d9d4bb
Chloe <gtalk.with.nani@gmail.com>
2024-02-08 08:54:05 +0900

    Default settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,64 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import { Lato, Noto_Sans_JP, Noto_Sans_KR } from 'next/font/google';
+import localFont from 'next/font/local';
+import '@/styles/globals.css';
+
+const fontKR = Noto_Sans_KR({
+  weight: ['400', '700'],
+  subsets: ['cyrillic'],
+});
+
+const fontEN = Lato({
+  weight: ['100', '300', '400', '700', '900'],
+  subsets: ['latin'],
+});
+
+const NanumSquare = localFont({
+  src: [
+    {
+      path: '../fonts/NanumSquareL.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/NanumSquareR.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/NanumSquareB.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/NanumSquareEB.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return;
+  <>
+    <style jsx global>
+      {`
+        body,
+        pre,
+        input,
+        button,
+        textarea,
+        select {
+          font-family: ${NanumSquare.style.fontFamily}, monospace;
+        }
+        .preview {
+          font-family: ${fontKR.style.fontFamily};
+        }
+        time {
+          font-family: ${fontEN.style.fontFamily};
+        }
+      `}
+    </style>
+    <Component {...pageProps} />;
+  </>;
 }
