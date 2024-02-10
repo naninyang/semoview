7548196d2245c634557edc3121a36a194a5011b7
Chloe <gtalk.with.nani@gmail.com>
2024-02-11 00:06:54 +0900

    Favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,33 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
+import { GA_TRACKING_ID } from '@/lib/gtag';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko-KR">
+      <Head>
+        <link color="#141414" href="/favicon/safari-pinned-tab.svg" rel="mask-icon" />
+        <link href="/favicon/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+        <link href="/favicon/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/favicon/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+        <link href="/favicon/favicon.ico" rel="shortcut icon" />
+        <link href="/manifest.json" rel="manifest" />
+        <meta name="application-name" content="제제없 JEJEUP" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="Black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="제제없 JEJEUP" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#141414" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#141414" />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${GA_TRACKING_ID}', { page_path: window.location.pathname });`,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
