64acef68494d840ef814a7eadc0956d313db64e1
Chloe <gtalk.with.nani@gmail.com>
2024-02-25 16:31:53 +0900

    Update AnimeName.tsx

--- components/AnimeName.tsx
+++ components/AnimeName.tsx
@@ -1,7 +1,7 @@
 export const AnimeName = (org: string) => {
   const names: { [key: string]: string } = {
     tva: 'TVA',
-    OVA: 'OVA',
+    ova: 'OVA',
     movie: '극장판',
   };
   return names[org] || '';
