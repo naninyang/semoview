cc229b55729c84a0c15d4e74baf7e0fd25649056
Chloe <gtalk.with.nani@gmail.com>
2024-03-21 01:03:23 +0900

    Update strapi.ts

--- utils/strapi.ts
+++ utils/strapi.ts
@@ -54,11 +54,9 @@ export async function getJejeupData(page?: number) {
   const pageCount = jejeupResponse.meta.pagination.pageCount;
   const jejeups = await Promise.all(
     rowsData.map(async (preview) => {
-      const jejeupMetaData = await fetchPreviewMetadata(`https://youtu.be/${preview.video}`);
       const amusementData = await getAmusementData(preview.title);
       return {
         ...preview,
-        jejeupMetaData,
         amusementData,
       };
     }),
